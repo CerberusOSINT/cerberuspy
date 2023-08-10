5496ac8d0d5050cd111046238ae882b8411cd870
cerberus <support@iscerberus.ru>
2023-08-10 16:12:51 +0500

    изменено:      setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
 	name='cerberuspy',
-	version='1.0.4',
+	version='1.0.5',
 	author='iscerberus',
 	author_email='support@iscerberus.ru',
 	description='Клиент для использования API ИС "Цербер".',
-        url='https://github.com/iscerberus/cerberuspy')
+        url='https://github.com/CerberusOSINT/cerberuspy')
